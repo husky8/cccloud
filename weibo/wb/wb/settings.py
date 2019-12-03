a8d36e661a9ee730c6726f2e8015b3ea1aff0bef
chencheng01 <chencheng01@smzdm.com>
2019-12-03 23:13:15 +0800

    no message

--- weibo/wb/wb/settings.py
+++ weibo/wb/wb/settings.py
@@ -88,3 +88,40 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+USER_AGENT_LIST = ['zspider/0.9-dev http://feedback.redkolibri.com/',
+                    'Xaldon_WebSpider/2.0.b1',
+                    'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) Speedy Spider (http://www.entireweb.com/about/search_tech/speedy_spider/)',
+                    'Mozilla/5.0 (compatible; Speedy Spider; http://www.entireweb.com/about/search_tech/speedy_spider/)',
+                    'Speedy Spider (Entireweb; Beta/1.3; http://www.entireweb.com/about/search_tech/speedyspider/)',
+                    'Speedy Spider (Entireweb; Beta/1.2; http://www.entireweb.com/about/search_tech/speedyspider/)',
+                    'Speedy Spider (Entireweb; Beta/1.1; http://www.entireweb.com/about/search_tech/speedyspider/)',
+                    'Speedy Spider (Entireweb; Beta/1.0; http://www.entireweb.com/about/search_tech/speedyspider/)',
+                    'Speedy Spider (Beta/1.0; www.entireweb.com)',
+                    'Speedy Spider (http://www.entireweb.com/about/search_tech/speedy_spider/)',
+                    'Speedy Spider (http://www.entireweb.com/about/search_tech/speedyspider/)',
+                    'Speedy Spider (http://www.entireweb.com)',
+                    'Sosospider+(+http://help.soso.com/webspider.htm)',
+                    'sogou spider',
+                    'Nusearch Spider (www.nusearch.com)',
+                    'nuSearch Spider (compatible; MSIE 4.01; Windows NT)',
+                    'lmspider (lmspider@scansoft.com)',
+                    'lmspider lmspider@scansoft.com',
+                    'ldspider (http://code.google.com/p/ldspider/wiki/Robots)',
+                    'iaskspider/2.0(+http://iask.com/help/help_index.html)',
+                    'iaskspider',
+                    'hl_ftien_spider_v1.1',
+                    'hl_ftien_spider',
+                    'FyberSpider (+http://www.fybersearch.com/fyberspider.php)',
+                    'FyberSpider',
+                    'everyfeed-spider/2.0 (http://www.everyfeed.com)',
+                    'envolk[ITS]spider/1.6 (+http://www.envolk.com/envolkspider.html)',
+                    'envolk[ITS]spider/1.6 ( http://www.envolk.com/envolkspider.html)',
+                    'Baiduspider+(+http://www.baidu.com/search/spider_jp.html)',
+                    'Baiduspider+(+http://www.baidu.com/search/spider.htm)',
+                    'BaiDuSpider',
+                    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0) AddSugarSpiderBot www.idealobserver.com',
+                   ]
+DOWNLOADER_MIDDLEWARES = {
+    'wb.MidWare.HeaderMidWare.ProcessHeaderMidware': 543,
+}
--- weibo/wb/wb/spiders/wb_spider.py
+++ weibo/wb/wb/spiders/wb_spider.py
@@ -1,42 +1,30 @@
-#encoding = "utf-8"
+# encoding = "utf-8"
 import scrapy
-import json
 from bs4 import BeautifulSoup
-from scrapy.selector import  Selector
-class WbSpider(scrapy.spiders.Spider):
 
+
+class WbSpider(scrapy.spiders.Spider):
     name = "weibo"
-    allowed_domains = ["s.weibo.com/"]
+    allowed_domains = ["s.weibo.com","captcha.weibo.com"]
     start_urls = [
-        "https://s.weibo.com/top/summary?cate=realtimehot"
+        "https://s.weibo.com/top/summary?cate=realtimehot".replace(" ","")
     ]
 
-    # def start_requests(self):
-    #     return [scrapy.FormRequest("http://www.example.com/login",
-    #                                # formdata={'user': 'john', 'pass': 'secret'},
-    #                                callback=self.parse())]
-
-    def parse(self,response):
+    def parse(self, response):
         rhtmls = response.xpath("//tbody/tr").extract()
         self.getData(rhtmls)
 
-    def getData(self,rhtmls):
+    def getData(self, rhtmls):
         for rhtml in rhtmls:
-            t = BeautifulSoup(rhtml,'lxml')
+            # print("-*100")
+            # print(rhtml)
+            # print("-*100")
+            t = BeautifulSoup(rhtml, 'lxml')
 
-            index = t.find("td",class_ = "td-01").string
-            keyword = t.find("td",class_="td-02").a.string
-            value = t.find("span").string
-            # href = t.find("p",class_="star_name").a.get("herf")
-            # isnew = t.find("p",class_="star_name").i
-            # if isnew != None:
-            #     isnew_str = isnew.string
-            # else:
-            #     isnew_str = ""
-            # searchs = t.find("p",class_="star_name").span.string
-            print(index,keyword,value
-                  # keyword,href,isnew_str,searchs
-                  )
-            print("-*100")
-if __name__ == '__main__':
-    WbSpider().start_requests()
+            index = t.find("td", class_="td-01").string
+            keyword = t.find("td", class_="td-02").a.string
+            value = t.find("td", class_="td-02").span
+            value = str(value).replace("<span>","").replace("</span>","")
+            # print(str(value).replace("<span>","").replace("</span>",""))
+            print(index, keyword,value)
+            print("-"*80)
